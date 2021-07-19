e6ffbd129ccabfbbe66a7aaab28328a2a398e3eb
Lee Jae Hyun <myleje@gmail.com>
2021-07-19 17:05:28 +0900

    .

--- Assets/02.Scripts/TankCtrl.cs
+++ Assets/02.Scripts/TankCtrl.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class TankCtrl : MonoBehaviour
 {
     private Transform tr;
+    private PhotonView pv;
 
     public float speed = 10.0f;
 
@@ -12,14 +14,18 @@ public class TankCtrl : MonoBehaviour
     void Start()
     {
         tr = GetComponent<Transform>();
+        pv = GetComponent<PhotonView>();
     }
 
     void Update()
     {
-        float v = Input.GetAxis("Vertical");
-        float h = Input.GetAxis("Horizontal");
+        if (pv.IsMine == true)
+        {
+            float v = Input.GetAxis("Vertical");
+            float h = Input.GetAxis("Horizontal");
 
-        tr.Translate(Vector3.forward * Time.deltaTime * speed * v);
-        tr.Rotate(Vector3.up * Time.deltaTime * 100.0f * h);
+            tr.Translate(Vector3.forward * Time.deltaTime * speed * v);
+            tr.Rotate(Vector3.up * Time.deltaTime * 100.0f * h);
+        }
     }
 }
