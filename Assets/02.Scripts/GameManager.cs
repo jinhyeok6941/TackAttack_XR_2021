bcaf806a5917b462e6bc480c43d1eeb78416fcc0
Lee Jae Hyun <myleje@gmail.com>
2021-07-20 15:15:24 +0900

    .

--- Assets/02.Scripts/GameManager.cs
+++ Assets/02.Scripts/GameManager.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
+using TMPro;
+using Photon.Realtime;
 
 public class GameManager : MonoBehaviourPunCallbacks
 {
+    public TMP_Text messageText;
+    public TMP_Text connectInfoText;
+
     void Awake()
     {
         Vector3 pos = new Vector3(Random.Range(-150.0f, 150.0f), 0.0f, Random.Range(-150.0f, 150.0f));
@@ -22,4 +27,17 @@ public class GameManager : MonoBehaviourPunCallbacks
     {
         SceneManager.LoadScene("Lobby");
     }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        string msg = $"\n<color=#00ff00>{newPlayer.NickName}</color> is joined room";
+        messageText.text += msg;
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        string msg = $"\n<color=#ff0000>{otherPlayer.NickName}</color> is left room";
+        messageText.text += msg;
+    }
+
 }
